35330414473311e6a9779105f075608668485a98
infectedvoltage <gekoglenn@hotmail.com>
2021-01-24 11:37:41 +0100

    developing autodiscover

--- autodiscover.sh
+++ autodiscover.sh
@@ -15,26 +15,15 @@ get_network_range() {
 
 
 scan_for_pis() {
-   #pi_ips=$(sudo nmap -sP $ip_clean | awk '/^Nmap/{ip=$NF}/B8:27:EB|DC:A6:32|E4:5F:01/{print ip}')
-   #clean_pi_ips=$(echo $pi_ips |sed 's/[()]//g')
-
-   #dirty file writes for tracing
-   #echo $clean_pi_ips > /tmp/pi_ips.log
-   #sed 's/\s\+/\n/g' /tmp/pi_ips.log > /tmp/pi_ips_clean.log
-
-   #cleaner array storage
    mapfile -t pi_ips < <(sudo nmap -sP $ip_clean | awk '/^Nmap/{ip=$NF}/B8:27:EB|DC:A6:32|E4:5F:01/{print ip}'|sed 's/[()]//g'|sed 's/\s\+/\n/g')
-
-
 }
 
 check_if_clusterable(){
-    while read line
-    do
-    #testing
-        echo $line
-        #ssh ubuntu@$line hostname
-    done < /tmp/pi_ips_clean.log
+for key in ${pi_ips[@]}
+do
+echo $key
+ssh ubuntu@$key hostname
+done
 }
 
 
@@ -44,9 +33,6 @@ scan_for_pis
 
 echo $ip_clean
 
-for key in ${pi_ips[@]}
-do
-echo $key
-done
+
 
 #check_if_clusterable
