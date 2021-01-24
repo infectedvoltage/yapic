4d0262b09e75ce79ab55c44c0bf96afea8ed4b4a
infectedvoltage <gekoglenn@hotmail.com>
2021-01-24 11:32:13 +0100

    developing autodiscover + adding notes

--- autodiscover.sh
+++ autodiscover.sh
@@ -4,6 +4,8 @@
 # this script will automatically discover any raspberry pi devices on your network, list them and create an ansible inventory file for them.
 #
 
+#init vars
+
 get_network_range() {
     ip_dirty=$(ip a | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9/]*).*/\2/p')
     ip_clean=$(echo $ip_dirty | cut -d " " -f 1)
@@ -13,10 +15,17 @@ get_network_range() {
 
 
 scan_for_pis() {
-   pi_ips=$(sudo nmap -sP $ip_clean | awk '/^Nmap/{ip=$NF}/B8:27:EB|DC:A6:32|E4:5F:01/{print ip}')
-   clean_pi_ips=$(echo $pi_ips |sed 's/[()]//g')
-   echo $clean_pi_ips > /tmp/pi_ips.log
-   sed 's/\s\+/\n/g' /tmp/pi_ips.log > /tmp/pi_ips_clean.log
+   #pi_ips=$(sudo nmap -sP $ip_clean | awk '/^Nmap/{ip=$NF}/B8:27:EB|DC:A6:32|E4:5F:01/{print ip}')
+   #clean_pi_ips=$(echo $pi_ips |sed 's/[()]//g')
+
+   #dirty file writes for tracing
+   #echo $clean_pi_ips > /tmp/pi_ips.log
+   #sed 's/\s\+/\n/g' /tmp/pi_ips.log > /tmp/pi_ips_clean.log
+
+   #cleaner array storage
+   mapfile -t pi_ips < <(sudo nmap -sP $ip_clean | awk '/^Nmap/{ip=$NF}/B8:27:EB|DC:A6:32|E4:5F:01/{print ip}'|sed 's/[()]//g'|sed 's/\s\+/\n/g')
+
+
 }
 
 check_if_clusterable(){
@@ -24,7 +33,7 @@ check_if_clusterable(){
     do
     #testing
         echo $line
-        ssh ubuntu@$line hostname
+        #ssh ubuntu@$line hostname
     done < /tmp/pi_ips_clean.log
 }
 
@@ -34,4 +43,10 @@ get_network_range
 scan_for_pis
 
 echo $ip_clean
-check_if_clusterable
+
+for key in ${pi_ips[*]}
+do
+echo "$key => ${pi_ips[$key]}"
+done
+
+#check_if_clusterable
